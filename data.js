0aa558f7cf141b0b78322b33609d421c23f189e4
Aleksandr Tikhonov <t9101029991@gmail.com>
2017-12-03 18:56:23 +0300

    start with express

---
+++ data.js
@@ -0,0 +1,49 @@
+module.exports =  [
+  {
+    title: 'Автовоз Volvo FH12',
+    description: '<div><p>Низкорамник: ширина &mdash; 2,55 м,&nbsp;длина &mdash; 17м.<br />Максимальная высота перевозимых автомобилей &mdash; 3,1м.<br />Осуществляет перевозку автомобилей коммерческого транспорта.</p></div>',
+    images: [
+      { thumbnail: 'product-1-1-thumbnail.jpg', image: 'product-1-1.jpg' },
+      { thumbnail: 'product-1-2-thumbnail.jpg', image: 'product-1-2.jpg' }
+    ]
+  },
+  {
+    title: 'Автовоз Scania R340',
+    description: '<p>Низкорамник: ширина &mdash; 2,55м,&nbsp;длина &mdash; 17м.<br />Максимальная высота перевозимых автомобилей &mdash; 3,2м.<br />Осуществляет перевозку автомобилей коммерческого транспорта.</p>',
+    images: [
+      { thumbnail: 'product-2-1-thumbnail.jpg', image: 'product-2-1.jpg' },
+      { thumbnail: 'product-2-2-thumbnail.jpg', image: 'product-2-2.jpg' },
+      { thumbnail: 'product-2-3-thumbnail.jpg', image: 'product-2-3.jpg' }
+    ]
+  },
+  {
+    title: 'Автовоз MAN',
+    description: '<p>Низкорамник: ширина &mdash; 2,55м,&nbsp;длина &mdash; 17м.<br />Максимальная высота перевозимых автомобилей &mdash; 3,6м.<br />Осуществляет перевозку автомобилей коммерческого транспорта,&nbsp;спецтехники, строительной техники</p>',
+    images: [
+      { thumbnail: 'product-3-1-thumbnail.jpg', image: 'product-3-1.jpg' },
+      { thumbnail: 'product-3-2-thumbnail.jpg', image: 'product-3-2.jpg' },
+      { thumbnail: 'product-3-3-thumbnail.jpg', image: 'product-3-3.jpg' }
+    ]
+  },
+  {
+    title: 'Автовоз-Эвакуатор Валдай',
+    description: '<p>Низкорамник:</p><ul><li>ширина &mdash; 2,55м</li><li>длина тягача &mdash; 5,5м</li><li>длина прицепа &mdash; 11м</li></ul><p>Максимальна масса не превышает 12т.<br />Проходной по МКАД и Москве</p>',
+    images: [
+      { thumbnail: 'product-4-1-thumbnail.jpg', image: 'product-4-1.jpg' },
+      { thumbnail: 'product-4-2-thumbnail.jpg', image: 'product-4-2.jpg' },
+      { thumbnail: 'product-4-3-thumbnail.jpg', image: 'product-4-3.jpg' }
+    ]
+  },
+  {
+    title: 'Эвакуатор HYUNDAI HD78',
+    description: '<ul><li>ширина &mdash; 2,4м</li><li>длина 7,5м</li><li>Максимальная грузоподъемность 3800кг.</li><li>Максимальная длина перевозимого&nbsp;автомобиля 7.7м,</li><li>Максимальная высота перевозимого автомобиля 3.3м</li></ul><p>Перевозит коммерческий транспорт. автомобили после ДТП</p>',
+    images: [
+      { thumbnail: 'product-5-1-thumbnail.jpg', image: 'product-5-1.jpg' },
+      { thumbnail: 'product-5-2-thumbnail.jpg', image: 'product-5-2.jpg' },
+      { thumbnail: 'product-5-3-thumbnail.jpg', image: 'product-5-3.jpg' },
+      { thumbnail: 'product-5-4-thumbnail.jpg', image: 'product-5-4.jpg' },
+      { thumbnail: 'product-5-5-thumbnail.jpg', image: 'product-5-5.jpg' },
+      { thumbnail: 'product-5-6-thumbnail.jpg', image: 'product-5-6.jpg' }
+    ]
+  }
+]
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ejs =  require('ejs');
+const path = require('path');
+const app = express();
+const data = require('./data.js');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join('./public')));
+
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, './view/index'));
+});
+
+app.get('/products', (req, res) => {
+  res.render(path.join(__dirname, './view/products'), {data: data});
+})
+
+app.get('/contacts', (req, res) => {
+  res.render(path.join(__dirname, './view/contacts'));
+})
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
