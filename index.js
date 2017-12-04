62c67a774ef81e2660d95f993d0cd3301d3bac60
Aleksandr Tikhonov <t9101029991@gmail.com>
2017-12-04 17:33:51 +0300

    single product

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join('./public')));
 
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname, './view/index'));
+  res.render(path.join(__dirname, './view/index'), {title: 'Нижавто. Услуги автовозов и эвакуаторов'});
 });
 
 app.get('/products', (req, res) => {
-  res.render(path.join(__dirname, './view/products'), {data: data});
+  res.render(path.join(__dirname, './view/products'), {title: 'Автовозы', data: data});
+})
+
+app.get('/products/:slug', (req, res) => {
+  let result = data.find(el => el.slug === req.params.slug);
+  result == 'undefined' ? res.send(404) : res.render(path.join(__dirname, './view/single'), {title: result.title, data: result});;
 })
 
 app.get('/contacts', (req, res) => {
-  res.render(path.join(__dirname, './view/contacts'));
+  res.render(path.join(__dirname, './view/contacts'), {title: 'Контакты'});
 })
 
 app.get('/distance', (req, res) => {
-  res.render(path.join(__dirname, './view/distance'));
+  res.render(path.join(__dirname, './view/distance'), {title: 'Расчет расстояний'});
 })
 
 app.listen(3000, () => {
