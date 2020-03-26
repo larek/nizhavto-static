51223f16b934a36ef7af17f43808fe279a6a5553
larek <t9101029991@gmail.com>
2020-03-26 18:49:41 +0300

    correct index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+/**
+ * Main config file
+ */
 const express = require('express');
-const ejs =  require('ejs');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 const data = require('./data.js');
@@ -8,24 +11,24 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join('./public')));
 
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname, './view/index'), {title: 'Нижавто. Услуги автовозов и эвакуаторов'});
+  res.render(path.join(__dirname, './view/index'), { title: 'Нижавто. Услуги автовозов и эвакуаторов' });
 });
 
 app.get('/products', (req, res) => {
-  res.render(path.join(__dirname, './view/products'), {title: 'Автовозы', data: data});
+  res.render(path.join(__dirname, './view/products'), { title: 'Автовозы', data: data });
 })
 
 app.get('/products/:slug', (req, res) => {
   let result = data.find(el => el.slug === req.params.slug);
-  result == 'undefined' ? res.send(404) : res.render(path.join(__dirname, './view/single'), {title: result.title, data: result});;
+  result == 'undefined' ? res.send(404) : res.render(path.join(__dirname, './view/single'), { title: result.title, data: result });;
 })
 
 app.get('/contacts', (req, res) => {
-  res.render(path.join(__dirname, './view/contacts'), {title: 'Контакты'});
+  res.render(path.join(__dirname, './view/contacts'), { title: 'Контакты' });
 })
 
 app.get('/distance', (req, res) => {
-  res.render(path.join(__dirname, './view/distance'), {title: 'Расчет расстояний'});
+  res.render(path.join(__dirname, './view/distance'), { title: 'Расчет расстояний' });
 })
 
 app.listen(3000, () => {
